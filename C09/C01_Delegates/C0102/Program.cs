6684de92f00b1a1e7d74f0bf8ce1099d82f5dcf0
Jin Hu <jinhuca@gmail.com>
2023-07-02 07:00:03 -0400

    fds

---
+++ C09/C01_Delegates/C0102/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace C0102
+{
+  public class Program
+  {
+    private static bool IsGreaterThanZero(int value) => value > 0;
+
+    public static void CreateADelegate()
+    {
+      var p = IsGreaterThanZero;
+      Console.WriteLine(p(42));
+    }
+
+    public static void Main(string[] args)
+    {
+      CreateADelegate();
+    }
+  }
+}
